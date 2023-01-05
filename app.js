0e06f6bc9ab153a7501c876b57d29c25bd24739d
marko.vukosav <marko.vukosav@rimac-automobili.com>
2023-01-05 19:18:31 +0100

    replace graphql with apollo server

--- app.js
+++ app.js
@@ -6,45 +6,57 @@ const getErrorCode = require("./helpers/getError.js")
 const grapqhlSchema = require("./graphql/schema/index");
 const graphqlResolvers = require("./graphql/resolvers/index");
 const { GraphQLError } = require("graphql");
+const { ApolloServer } = require("apollo-server-express");
 
+  async function startServer() {
+ 
+    const app = express();
 
-const app = express();
+    app.use(bodyParser.json());
 
-app.use(bodyParser.json());
+    app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "POST, GET,OPTIONS");
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+      );
+      if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+      }
+      next();
+    });
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
+    const apolloServer = new ApolloServer({
+    typeDefs: grapqhlSchema,
+    resolvers: graphqlResolvers,
+    formatError: (err) => {
+      const error = getErrorCode(err.message);
+      return { message: error.message, statusCode: error.statusCode };
+    }
+  });
 
-app.use(
-  "/graphql",
-  graphqlHttp.graphqlHTTP({
-    schema: grapqhlSchema,
-    rootValue: graphqlResolvers,
-    graphiql: true,
-    customFormatErrorFn: (error) => {
-      return new GraphQLError(error.message); 
-    },
-  })
-);
+    await apolloServer.start();
+
+    apolloServer.applyMiddleware({ app: app });
+
+    app.use((req, res) => {
+    res.send('Hello from express appolo server');
+  });
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(
-   process.env.MONGO_DB_URL, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      autoIndex: true,
-    }
-  )
+  .connect(process.env.MONGO_DB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    autoIndex: true,
+  })
   .then(() => {
     app.listen(8000);
+    console.log("mongoose connected");
   })
   .catch((err) => {
     console.log(err);
   });
+}
+
+startServer();
--- graphql/resolvers/movie.js
+++ graphql/resolvers/movie.js
@@ -6,55 +6,65 @@ const { errorName } = require("../../helpers/constants");
 const { GraphQLError} = require("graphql")
 
 module.exports = {
-  movies: async () => {
-    try {
-      const result = await Movie.find();
-      return result;
-    } catch (error) {
-      throw error;
-    }
-  },
-  movieById: async ({movieId}) => {
-    try {
-      const result = await Movie.findById(movieId);
-      return result;
-    } catch (error) {
-      throw error;
-    }
-  },
-  createMovie: async (args) => {
-    console.log(args.movieInput.category);
-    const movie = new Movie({
-      title: args.movieInput.title,
-      description: args.movieInput.description,
-      img: args.movieInput.img,
-      url: args.movieInput.url,
-      duration: args.movieInput.duration,
-      releaseDate: new Date(args.movieInput.releaseDate),
-      rating: args.movieInput.rating,
-      category: args.movieInput.category
-    });
-
-    try {
-      const result = await movie.save();
-      return result;
-    } catch (error) {
-      throw error;
-    }
-  },
-  addToFavorite: async({movieId, userId}) =>{
-      const user = await User.findById(userId);
-      const movie = await Movie.findById(movieId);
-      if (!user || !movie) {
-         throw customError("Unable to find movie", 404);
+  RootQuery: {
+    movies: async () => {
+      try {
+        const result = await Movie.find();
+        return result;
+      } catch (error) {
+        throw error;
       }
-      const checkDuplicate = await User.exists({ favoriteMovies: movieId });
-      if (checkDuplicate) {
-         throw new GraphQLError("deadline passed");
+    },
+    movieById: async ({ movieId }) => {
+      try {
+        const result = await Movie.findById(movieId);
+        return result;
+      } catch (error) {
+        throw error;
       }
+    },
+  },
+  RootMutation: {
+    createMovie: async (args) => {
+      console.log(args.movieInput.category);
+      const movie = new Movie({
+        title: args.movieInput.title,
+        description: args.movieInput.description,
+        img: args.movieInput.img,
+        url: args.movieInput.url,
+        duration: args.movieInput.duration,
+        releaseDate: new Date(args.movieInput.releaseDate),
+        rating: args.movieInput.rating,
+        category: args.movieInput.category,
+      });
 
-      user.favoriteMovies.push(movie);
-      await user.save();
-      return movie;
-  }
+      try {
+        const result = await movie.save();
+        return result;
+      } catch (error) {
+        throw error;
+      }
+    },
+
+    addToFavorite: async (_p,{ movieId, userId },_c,_i) => {
+      try {
+        console.log(movieId)
+        const user = await User.findById(userId);
+        const movie = await Movie.findById(movieId);
+        if (!user || !movie) {
+           throw new Error(errorName.USER_ALREADY_EXISTS); 
+        }
+        const checkDuplicate = await User.exists({ favoriteMovies: movieId });
+        if (checkDuplicate) {
+             throw new Error(errorName.USER_ALREADY_EXISTS);
+      
+        }
+        user.favoriteMovies.push(movie);
+        await user.save();
+        return movie;
+      } catch (error) {
+        throw error;
+      }
+    },
+  },
 };
