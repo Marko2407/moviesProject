498673adc3a50e6cd0ab5aa51ecb0b32ee063063
marko.vukosav <marko.vukosav@rimac-automobili.com>
2023-01-18 19:36:09 +0100

    a

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -6,109 +6,108 @@ const { transformMovies, transformUser } = require("../resolvers/merge");
 const { errorName } = require("../../helpers/constants");
 const { GraphQLError } = require("graphql");
 
-
 function generateAccessToken(userId) {
-  return jwt.sign({ userId: userId }, process.env.SECURE_ACCESS_TOKEN_KEY, {
-    expiresIn: "30s",
-  });
+	return jwt.sign({ userId: userId }, process.env.SECURE_ACCESS_TOKEN_KEY, {
+		expiresIn: "1h",
+	});
 }
 
 const authResolver = {
-  RootQuery: {
-    user: async () => {
-      const users = await User.find();
+	RootQuery: {
+		user: async () => {
+			const users = await User.find();
 
-      users.map((user) => {
-        transformUser(user);
-      });
-      return users;
-    },
-    login: async (_p, { email, password }, _c, _i) => {
-      try {
-        const user = await User.findOne({ email: email });
-        if (!user) {
-          throw new Error(errorName.USER_NOT_FOUND);
-         }
-        const isEqual = await bcrypt.compare(password, user.password);
-        if (!isEqual) {
-            throw new Error(errorName.UNKNOWN);
-       }
-        const token = generateAccessToken(user.id);
-        const refreshToken = jwt.sign(
-          user.id,
-          process.env.SECURE_REFRESH_TOKEN_KEY
-        );
-        await User.findByIdAndUpdate(user.id, { refreshToken: refreshToken });
-        return {
-          userId: user.id,
-          token: token,
-          refreshToken: refreshToken,
-          tokenExpired: 1,
-        };
-      } catch (error) {
-        throw error;
-      }
-    },
+			users.map((user) => {
+				transformUser(user);
+			});
+			return users;
+		},
+		login: async (_p, { email, password }, _c, _i) => {
+			try {
+				const user = await User.findOne({ email: email });
+				if (!user) {
+					throw new Error(errorName.USER_NOT_FOUND);
+				}
+				const isEqual = await bcrypt.compare(password, user.password);
+				if (!isEqual) {
+					throw new Error(errorName.UNKNOWN);
+				}
+				const token = generateAccessToken(user.id);
+				const refreshToken = jwt.sign(
+					user.id,
+					process.env.SECURE_REFRESH_TOKEN_KEY
+				);
+				await User.findByIdAndUpdate(user.id, { refreshToken: refreshToken });
+				return {
+					userId: user.id,
+					token: token,
+					refreshToken: refreshToken,
+					tokenExpired: 1,
+				};
+			} catch (error) {
+				throw error;
+			}
+		},
 
-    userInfo: async (_p, args, _c, _i) => {
-      try {
-        const user = await User.findById(args.userId.toString());
-        if (!user) {
-          throw new Error(errorName.USER_NOT_FOUND);
-        }
-        return transformUser(user);
-      } catch (err) {
-         throw err;
-      }
-    },
-  },
-  RootMutation: {
-    createUser: async (_ep, args, _ec, _ei) => {
-      try {
-        const existingUser = await User.findOne({
-          email: args.userInput.email,
-        });
-        if (existingUser) {
-         throw new Error(errorName.USER_ALREADY_EXISTS);
-        }
-        const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
-        const user = new User({
-          fullName: args.userInput.fullName,
-          email: args.userInput.email,
-          password: hashedPassword,
-          refreshToken: null,
-        });
-        const result = await user.save();
-        return { ...result._doc, password: null };
-      } catch (err) {
-        throw err
-      }
-    },
-    newAccessToken: async (_p, args, _c, _i) => {
-      try {
-        const user = await User.findById(args.userId);
-        if (!user) {
-              throw new Error(errorName.USER_ALREADY_EXISTS);
-        }
-        if (user.refreshToken === args.refreshToken) {
-          return jwt.verify(
-            user.refreshToken,
-            process.env.SECURE_REFRESH_TOKEN_KEY,
-            (err, user) => {
-              if (err) throw new Error(errorName.UNKNOWN);
-              token = generateAccessToken(user);
-              return {
-                token: token,
-                expired: 1,
-              };
-            }
-          );
-        }
-      } catch (error) {
-          throw error;
-      }
-    },
-  },
+		userInfo: async (_p, args, _c, _i) => {
+			try {
+				const user = await User.findById(args.userId.toString());
+				if (!user) {
+					throw new Error(errorName.USER_NOT_FOUND);
+				}
+				return transformUser(user);
+			} catch (err) {
+				throw err;
+			}
+		},
+	},
+	RootMutation: {
+		createUser: async (_ep, args, _ec, _ei) => {
+			try {
+				const existingUser = await User.findOne({
+					email: args.userInput.email,
+				});
+				if (existingUser) {
+					throw new Error(errorName.USER_ALREADY_EXISTS);
+				}
+				const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+				const user = new User({
+					fullName: args.userInput.fullName,
+					email: args.userInput.email,
+					password: hashedPassword,
+					refreshToken: null,
+				});
+				const result = await user.save();
+				return { ...result._doc, password: null };
+			} catch (err) {
+				throw err;
+			}
+		},
+		newAccessToken: async (_p, args, _c, _i) => {
+			try {
+				const user = await User.findById(args.userId);
+				if (!user) {
+					throw new Error(errorName.USER_ALREADY_EXISTS);
+				}
+				if (user.refreshToken === args.refreshToken) {
+					return jwt.verify(
+						user.refreshToken,
+						process.env.SECURE_REFRESH_TOKEN_KEY,
+						(err, user) => {
+							if (err) throw new Error(errorName.UNKNOWN);
+							token = generateAccessToken(user);
+							return {
+								token: token,
+								expired: 1,
+							};
+						}
+					);
+				}
+			} catch (error) {
+				throw error;
+			}
+		},
+	},
 };
 
-module.exports = authResolver
+module.exports = authResolver;
